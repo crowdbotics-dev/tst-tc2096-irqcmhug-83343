c7d52eb3563cd62827388a419953925d5ff20c8b
Kushal Karki <kushal@crowdbotics.com>
2023-05-15 15:14:15 +0545

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "tst-tc2096-irqcmhug-83343",
-  url: "https://tst-tc2096-irqcmhug-83343.botics.co",
-  api: "https://tst-tc2096-irqcmhug-83343.botics.co/api/v1"
-}
+  "name": "tst-tc2096-irqcmhug-83343",
+  "url": "https://tst-tc2096-irqcmhug-83343.botics.co",
+  "api": "https://tst-tc2096-irqcmhug-83343.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
